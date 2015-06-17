0b5079af269ac07ec27c0e7556575b2708269ecb
Igor de Lorenzi Andrade <igordelorenzi@gmail.com>
2015-06-17 16:25:42 -0300

    Initial Commit

---
+++ TimetableCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimetableCollectionView
+//
+//  Created by Igor Andrade on 6/17/15.
+//  Copyright (c) 2015 Tokenlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TimetableCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimetableCollectionView
+//
+//  Created by Igor Andrade on 6/17/15.
+//  Copyright (c) 2015 Tokenlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
