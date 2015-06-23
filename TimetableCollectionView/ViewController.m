5c33fbba379bbc9bec1e543639f4fe18e98a3bf6
Igor de Lorenzi Andrade <igordelorenzi@gmail.com>
2015-06-23 14:21:23 -0300

    Further customizations.

--- TimetableCollectionView/ViewController.m
+++ TimetableCollectionView/ViewController.m
@@ -39,10 +39,10 @@ static NSString *timeCellIdentifier = @"TimeCellIdentifier";
     self.flattenData = @[
         @[@0,       @"SEG", @"TER",                                @"QUA",                                           @"QUI",                                @"SEX"],
         @[@"19:20", @0,     @"SEMINÁRIOS INTEGRADOS EM MARKETING", @0,                                               @"SEMINÁRIOS INTEGRADOS EM MARKETING", @0],
-        @[@"20:10", @0,     @0,                                    @0,                                               @0,                                    @0],
-        @[@"21:00", @0,     @0,                                    @0,                                               @0,                                    @0],
+        @[@"20:10", @0,     @1,                                    @0,                                               @1,                                    @0],
+        @[@"21:00", @0,     @1,                                    @0,                                               @0,                                    @0],
         @[@"21:30", @0,     @0,                                    @"PREVISAO E MENSURAÇÃO DA DEMANDA EM MARKETING", @0,                                    @0],
-        @[@"22:00", @0,     @0,                                    @0,                                               @0,                                    @0]
+        @[@"22:00", @0,     @0,                                    @1,                                               @0,                                    @0]
     ];
 }
 
@@ -62,6 +62,8 @@ static NSString *timeCellIdentifier = @"TimeCellIdentifier";
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    id item = self.flattenData[indexPath.section][indexPath.row];
+    
     if (indexPath.section == 0) {
         WeekDayCollectionViewCell *weekDayCell = (WeekDayCollectionViewCell *)[collectionView dequeueReusableCellWithReuseIdentifier:weekDayCellIdentifier forIndexPath:indexPath];
         if (indexPath.row == 0) {
@@ -69,36 +71,31 @@ static NSString *timeCellIdentifier = @"TimeCellIdentifier";
             [weekDayCell.weekDayLabel setEnabled:false];
             weekDayCell.weekDayLabel.text = @"";
         } else {
-            weekDayCell.weekDayLabel.text = self.flattenData[indexPath.section][indexPath.row];
+            weekDayCell.weekDayLabel.text = (NSString *)item;
         }
-//        weekDayCell.weekDayLabel.layer.borderWidth=1.0f;
-//        weekDayCell.weekDayLabel.layer.borderColor=[UIColor blueColor].CGColor;
         return weekDayCell;
     } else {
         if (indexPath.row == 0) {
             TimeCollectionViewCell *timeCell = (TimeCollectionViewCell *)[collectionView dequeueReusableCellWithReuseIdentifier:timeCellIdentifier forIndexPath:indexPath];
             timeCell.timeLabel.text = self.flattenData[indexPath.section][indexPath.row];
-//            timeCell.timeLabel.layer.borderWidth=1.0f;
-//            timeCell.timeLabel.layer.borderColor=[UIColor blueColor].CGColor;
             return timeCell;
         } else {
             EventCollectionViewCell *eventCell = (EventCollectionViewCell *)[collectionView dequeueReusableCellWithReuseIdentifier:eventCellIdentifier forIndexPath:indexPath];
-            if ([self.flattenData[indexPath.section][indexPath.row] isEqual:@0]) {
+            if ([item isEqual:@0] || [item isEqual:@1]) {
                 eventCell.eventLabel.text = @"";
                 eventCell.eventLabel.backgroundColor = [UIColor whiteColor];
             } else {
-                eventCell.eventLabel.text = self.flattenData[indexPath.section][indexPath.row];
+                eventCell.eventLabel.text = (NSString *)item;
                 eventCell.eventLabel.backgroundColor = [UIColor colorWithRed:114.f/255.f green:184.f/255.f blue:175.f/255.f alpha:1.0f];
             }
-            eventCell.eventLabel.layer.borderWidth=1.0f;
-            eventCell.eventLabel.layer.borderColor=[UIColor blackColor].CGColor;
             return eventCell;
         }
     }
 }
 
-// TODO: retirar parametros nao usados
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    NSInteger columnSpan = 1;
+    id item = self.flattenData[indexPath.section][indexPath.row];
     if (indexPath.section == 0) {
         if (indexPath.row == 0) {
             return CGSizeMake(75.f, 40.f);
@@ -109,16 +106,18 @@ static NSString *timeCellIdentifier = @"TimeCellIdentifier";
         if (indexPath.row == 0) {
             return CGSizeMake(75.f, 120.f);
         } else {
-            if ([self.flattenData[indexPath.section][indexPath.row] isEqual:@0]) {
+            if ([item isEqual:@0] || [item isEqual:@1]) {
                 return CGSizeMake(120.f, 120.f);
             } else {
-                return CGSizeMake(120.f, 240.f);
+                columnSpan = [self columnSpanForItemAtIndexPath:indexPath];
+                return CGSizeMake(120.f, columnSpan * 120.f);
             }
         }
     }
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout zIndexForItemAtIndexPath:(NSIndexPath *)indexPath {
+    id item = self.flattenData[indexPath.section][indexPath.row];
     if (indexPath.section == 0) {
         if (indexPath.row == 0) {
             return 1024;
@@ -129,13 +128,26 @@ static NSString *timeCellIdentifier = @"TimeCellIdentifier";
         if (indexPath.row == 0) {
             return 1023;
         } else {
-            if ([self.flattenData[indexPath.section][indexPath.row] isEqual:@0]) {
+            if ([item isEqual:@0] || [item isEqual:@1]) {
                 return 0;
             } else {
                 return 1;
             }
         }
     }
+}
+
+- (NSInteger)columnSpanForItemAtIndexPath:(NSIndexPath *)indexPath {
+    NSInteger maxNumberOfSections = [self numberOfSectionsInCollectionView:self.collectionView];
+    NSInteger colSpan = 1;
+    NSInteger section = indexPath.section + 1;
+    
+    while (section < maxNumberOfSections && [self.flattenData[section][indexPath.row] isEqual:@1]) {
+        colSpan++;
+        section++;
+    }
+    
+    return colSpan;
 }
 
 - (void)didReceiveMemoryWarning {
